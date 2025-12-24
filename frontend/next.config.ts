18a5f59dc13522df6811380a326b11bd2fb611f7
Danu Suko Handiyanto <zpnever7@gmail.com>
2025-12-24 13:38:21 +0700

    update: fix error

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	allowedDevOrigins: ["103.103.22.103"],
+	async rewrites() {
+		return [
+			{
+				source: "/api/:path*",
+				destination: "http://103.103.22.103:4001/api/:path*",
+			},
+		];
+	},
 };
 
 export default nextConfig;
